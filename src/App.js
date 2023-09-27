08053f74ce17e7317ef7977248323e8d9e179a8a
hafeez <hafeezkhan123>
2023-09-27 20:45:38 +0500

    new change

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logIn from "./components/login/logIn";
 
 function App() {
   return (
+<>
+  <logIn/>
+</>
 
-
-    <div className="App">
-
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        kkk
-          Learn Reactmmmmmmjjjjjj
-
-
-          
-        </a>
-      </header>
-
-
-    </div>
   );
 }
 
